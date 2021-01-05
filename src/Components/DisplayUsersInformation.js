760540d46540a738f4a95e158122963d54507edc
ari-sarkar <arisarkar1995@gmail.com>
2021-01-06 02:24:45 +0530

    everything is done only pagination is left

--- src/Components/DisplayUsersInformation.js
+++ src/Components/DisplayUsersInformation.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TakeUserInformation from "./TakeUserInformation";
 import Navigation from "./Navigation";
 import "../Styles/DisplayUsersInformation.scss";
+import trash from "../Media/delete.svg"
 const DisplayUsersInformation = () => {
+  const [ isBtnClicked, setIsBtnClicked ] = useState(false)
+  const [ userData, setUserData ] = useState([])
+  const [ btnCliked, setBtnClicked ] = useState(false)
+
+  ////////useEffect
+  useEffect(() => {
+    const getData = localStorage.getItem("data")
+    if(getData) setUserData(JSON.parse(getData))
+  },[])
+  
+////////functions
+  const deleteDatafromLocale= (e) => {
+    console.log(e.target.value)
+    let index = e.target.value
+    userData.splice(index,1)
+    localStorage.setItem("data", JSON.stringify(userData))
+    setBtnClicked(!btnCliked)
+  }
   return (
     <section id="nav-user__display__input-container">
       <Navigation />
       <div id="display-user-wrapper">
-      <TakeUserInformation />
+      <TakeUserInformation isBtnClicked={isBtnClicked} setIsBtnClicked={setIsBtnClicked}/>
         <div className="add-user-btn">
-          <button>Add User</button>
+          <button onClick={e => setIsBtnClicked(!isBtnClicked)}>Add User</button>
         </div>
         <div className="display-user-table">
           <table>
@@ -20,13 +39,15 @@ const DisplayUsersInformation = () => {
               <th>Role</th>
               <th></th>
             </tr>
-            <tr className="table-row">
-              <td>1</td>
-              <td>Alfreds Futterkiste</td>
-              <td>Maria Anders</td>
-              <td>Germany</td>
-              <td></td>
-            </tr>
+            {userData.length ? userData.map((item, index) => (
+              <tr className="table-row" key={index+1}>
+                <td>{index+1}</td>
+                <td>{item.email}</td>
+                <td>        </td>
+                <td>{item.role}</td>
+                <td><button className="trash-btn-wrapper" value={index} onClick={deleteDatafromLocale}><img className="trash-btn" src={trash} alt="trash"></img></button></td>
+              </tr>
+            )) : <div className="nodata">NO User Data</div>}
           </table>
         </div>
       </div>
--- src/Components/TakeUserInformation.js
+++ src/Components/TakeUserInformation.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/TakeUserInformation.scss";
 import userLogo from "../Media/add-user.svg";
-const TakeUserInformation = () => {
+const TakeUserInformation = ({ isBtnClicked, setIsBtnClicked }) => {
+/////////////state
+const[ email, setEmail ] = useState("")
+const[ role, setRole ] = useState("Admin")
+//const [ btnCliked, setBtnClicked ] = useState(false)
+//////useEffect
+
+
+//////////functions
+    const clickedOnCancel=(e) =>{
+        e.preventDefault()
+        setEmail("")
+        setIsBtnClicked(!isBtnClicked)
+    }
+
+    const setDataInLocalStorage=(e) => {
+            let newArrOne=[]
+            let newArrTwo=[]
+            if(localStorage.getItem("data")){
+                newArrOne = JSON.parse(localStorage.getItem("data"))
+                if(newArrOne)newArrOne.push({email: email, role: role})
+                localStorage.setItem("data",JSON.stringify(newArrOne))
+            }else{
+                newArrTwo.push({email: email, role: role})
+                localStorage.setItem("data",JSON.stringify(newArrTwo))
+            }
+    }
+
   return (
-    <div id="user-logo__information-container">
+    <div className={ isBtnClicked ? "user-logo__information-container": "user-logo__information-container hide"}>
       <div className="user-logo-description-wrapper">
         <img src={userLogo} alt="user-logo"></img>
         <p>
@@ -15,21 +42,20 @@ const TakeUserInformation = () => {
       <div className="user-informmation-container">
         <div className="user-informmation-wrapper">
           <h5>User Information</h5>
-          <form>
+          <form onSubmit={setDataInLocalStorage}>
             <label>Email Id of User</label>
             <br />
-            <input></input><br />
+            <input type="email" required onChange={e => setEmail(e.target.value)} value={email}></input><br />
             <label>Role</label><br />
-            <select id="roles">
-              <option value="volvo">Admin</option>
-              <option value="saab">Owner</option>
-              <option value="opel">Sales</option>
+            <select id="roles" type="text" required onChange={e => setRole(e.target.value)} value={role}>
+              <option value="Admin">Admin</option>
+              <option value="Owner">Owner</option>
+              <option value="Sales">Sales</option>
             </select><br />
             <div className="btn-wrapper">
-            <button id="cancel">Cancel</button>
-            <button id="add">Add</button>
+            <button id="cancel" onClick={clickedOnCancel}>Cancel</button>
+            <button id="add" type="submit">Add</button>
             </div>
-            
           </form>
         </div>
       </div>
